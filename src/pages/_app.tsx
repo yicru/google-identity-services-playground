6aa9dd03230b67c4a8b6ff4796dd870f0421bf09
yuichiro miyamae <yu1ro.mya@gmail.com>
2023-05-23 15:18:54 +0900

    feat: Add urql package and configure urql client in _app.tsx
    Add urql package to package.json and configure urql client in _app.tsx file using Client, cacheExchange, and fetchExchange from urql package to handle GraphQL queries and mutations. Update package-lock.json accordingly.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,20 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import { Client, cacheExchange, fetchExchange, Provider } from 'urql'
+
+const client = new Client({
+  url: '/api/graphql',
+  exchanges: [cacheExchange, fetchExchange],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
       <ChakraProvider>
-        <Component {...pageProps} />
+        <Provider value={client}>
+          <Component {...pageProps} />
+        </Provider>
       </ChakraProvider>
     </GoogleOAuthProvider>
   )
