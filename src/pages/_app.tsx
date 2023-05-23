bc8c8e03a04d23a9f17d7aabd319f11169f417e5
yuichiro miyamae <yu1ro.mya@gmail.com>
2023-05-23 15:09:22 +0900

    feat: add @react-oauth/google for Google OAuth authorization
    Add @react-oauth/google package to utilize Google OAuth authorization
    with Google Identity Services Playground. GoogleOAuthProvider component
    is added to _app.tsx and GoogleLogin component is added to index.tsx.
    
    The NEXT_PUBLIC_GOOGLE_CLIENT_ID environment variable is required for
    GoogleOAuthProvider to work properly. An example .env file is provided.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </GoogleOAuthProvider>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,28 @@
-import { Center, Text } from '@chakra-ui/react'
+import { Container, Divider, Grid, Text } from '@chakra-ui/react'
+import { GoogleLogin } from '@react-oauth/google'
 
 export default function Home() {
   return (
-    <Center>
-      <Text>Hello Chakra</Text>
-    </Center>
+    <Container as={Grid} gap={4} py={8}>
+      <Text fontSize={'2xl'} fontWeight={'semibold'}>
+        Google Identity Services Playground
+      </Text>
+
+      <Divider />
+
+      <Grid gap={2}>
+        <Text fontWeight={'semibold'} color={'gray.600'}>
+          Sign In With Google
+        </Text>
+        <GoogleLogin
+          onSuccess={() => {
+            alert('Login Success')
+          }}
+          onError={() => {
+            alert('Login Failed')
+          }}
+        />
+      </Grid>
+    </Container>
   )
 }
